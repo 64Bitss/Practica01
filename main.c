6b0fccea3b6e6bb25db76c9efb261c37746c3e69
64Bitss <32716222+64Bitss@users.noreply.github.com>
2017-10-11 13:46:14 -0500

    MetodoBurbuja1

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+int main ()
+{
+int ayudante,x,y,z;
+int n=10,n_arreglo[n];
 
+for (x=0; x<n; x++);
+   {
+     printf("Ingrese numero: %d \n",x+1);
+     scanf("%d",&n_arreglo);
+   }
+for (x=0;x<n;x++){
+                        for (y=0;y<n-x;y++);
+ 	{
+		if (n_arreglo[y]>=n_arreglo[y+1]);
+		{
+			ayudante=n_arreglo[y];
+			n_arreglo[y]=n_arreglo[y+1];
+			n_arreglo[y+1]=ayudante;
+		}
+
+    }
+
+}
+  for (z=0;z<n;z++){ 
+   printf(" %d",n_arreglo[z]);
+}
+
+
+
+return 0;
+
+ }
